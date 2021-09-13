4883844300386c0dd1c510a60a783828b0a4bcf5
Enthusiast <77145252+EnthusiastNT@users.noreply.github.com>
2021-09-13 22:17:44 +0200

    Update essentials.php

--- essentials.php
+++ essentials.php
@@ -1,6 +1,55 @@
 <?php
 
 /**
+ * sanitize input from _GET or _POST
+ *
+ * @param string $name	input name
+ * @param int $limit	0 for unlimited
+ * @param bool $a2z		and digits and space [a-zA-Z0-9 ]
+ *
+ * @return string		sanitized input
+ */
+function func_GetInput($name, $limit = 0, $a2z = false)
+{
+	$value = '';
+	if( isset($_GET[$name]) )
+	{
+		$value = $_GET[$name];
+		$value = rawurldecode($value);  // urldecode would also replace + with Space
+	}
+	else if( isset($_POST[$name]) )
+	{
+		$value = $_POST[$name];
+	}
+	else
+	{
+		return '';
+	}
+
+	// only a-z and 0-9 and space
+	if( $a2z )
+	{
+		$value = mb_ereg_replace('[^A-Za-z0-9 ]', '', $value);
+		$value = mb_convert_case($value, MB_CASE_LOWER);
+	}
+	else
+	{
+		// basics
+		$value = htmlspecialchars($value, ENT_QUOTES);  // replace < > with &lt; &gt;
+		$value = mb_replace(["\r\n", "\r"], "\n", $value);
+	}
+
+	$value = trim($value);
+	if( $limit > 0 )
+	{
+		$value = mb_substr($value, 0, $limit);
+	}
+	$value = trim($value);
+
+	return $value;
+}
+
+/**
  * multi-byte version of str_replace, handles arrays in pattern and replacement
  *
  * @use: mb_replace(['<', '>', '"', '\''], ['&lt;', '&gt;', '&quot;', '&#39;'], $str);
